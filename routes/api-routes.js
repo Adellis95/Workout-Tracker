24521f54629ecaf67922884fa37d94911da7021f
Austin Ellis <Austin@Janets-iMac.local>
2021-01-09 18:05:27 -0500

    Added update route to add exercise to workout and a get route to get workouts in a range

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -31,5 +31,31 @@ module.exports = function(app) {
         });
     });
 
-    
+    app.put("/api/workouts/:id", (req, res) => {
+
+        db.Workout.findOneAndUpdate(
+            { _id: req.params.id },
+            {
+                $inc: { totalDuration: req.body.duration },
+                $push: { exercises: req.body }
+            },
+            { new: true }
+        ).then(dbWorkout => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        });
+    });
+
+    app.get("/api/workouts/range", (req, res) => {
+
+        db.Workout.find({}).then(dbWorkout => {
+            console.log("All Workouts");
+            console.log("dbWorkout");
+
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        });
+    });
 }
