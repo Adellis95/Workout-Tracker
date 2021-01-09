8b8a23ad8a1a0cd7b63e1d5faad34059f446203f
Austin Ellis <Austin@Janets-iMac.local>
2021-01-09 16:20:12 -0500

    Added post route for creating workouts and a get route to get the workouts

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,1 +1,35 @@
 const db = require("../models");
+
+module.exports = function(app) {
+
+    app.get("/api/workouts", (req, res) => {
+
+        db.Workout.find({}).then(dbWorkout => {
+            dbWorkout.forEach(workout => {
+                let total = 0;
+                workout.exercise.forEach(e => {
+                    console.log("Exercise");
+                    console.log(e);
+                    total += e.duration;
+                });
+                workout.totalDuration = total;
+            });
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        });
+    });
+
+    app.post("/api/workouts", ({ body }, res) => {
+        console.log("Workout to be added!!");
+        console.log(body);
+
+        db.Workout.create(body).then((dbWorkout => {
+            res.json(dbWorkout);
+        })).catch(err => {
+            res.json(err);
+        });
+    });
+
+    
+}
